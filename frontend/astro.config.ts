189e8c9f84b769a973ef6e06d2752a9259158503
oct-sky-out <kds3335k@icloud.com>
2025-06-24 12:11:59 +0900

    feat: add astrobook integration and update configuration

--- frontend/astro.config.ts
+++ frontend/astro.config.ts
@@ -5,10 +5,22 @@ import UnpluginTypia from "@ryoppippi/unplugin-typia/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 import checker from "vite-plugin-checker";
+import astrobook from "astrobook";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), vue()],
+  site: "https://iowiki.com",
+  base: "/",
+  output: "static",
+  integrations: [
+    react(),
+    vue(),
+    astrobook({
+      directory: "src",
+      head: "src/core/layouts/Layout.astro",
+      subpath: "/docs/components",
+    }),
+  ],
   vite: {
     plugins: [
       checker({
